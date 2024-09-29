5d430751bc136c33d0aeca8766508651b5be9130
Nishant <nishkohli96@gmail.com>
2024-09-30 03:25:43 +0530

    read swagger files

--- apps/server/src/docs/swagger.ts
+++ apps/server/src/docs/swagger.ts
@@ -3,20 +3,41 @@ import fs from 'fs';
 import path from 'path';
 import YAML from 'yaml';
 
-// async function readFiles() {
-const file = fs.readFileSync(path.join(__dirname + '/paths/test.yml'), 'utf-8');
-const file2 = fs.readFileSync(
-  path.join(__dirname + '/paths/test2.yml'),
-  'utf-8'
-);
+let swaggerPaths = {};
+let responses = {};
+let schemas = {};
+let securitySchemes = {};
 
-const f1 = YAML.parse(file);
-const f2 = YAML.parse(file2);
-const paths = {
-  ...f1.paths,
-  ...f2.paths,
-};
-console.log('paths: ', paths);
+async function readYMLFiles() {
+  /* Responses */
+  const responseData = fs.readFileSync(
+    path.join(__dirname + '/components/response.yml'),
+    'utf-8'
+  );
+  responses = YAML.parse(responseData);
+
+  /* Security Schema */
+  const securitySchemaData = fs.readFileSync(
+    path.join(__dirname + '/components/security-scheme.yml'),
+    'utf-8'
+  );
+  securitySchemes = YAML.parse(securitySchemaData);
+
+  /* API Paths */
+  const files = fs.readdirSync(path.join(__dirname + '/paths'));
+  files.forEach((file) => {
+    const fileContent = fs.readFileSync(
+      path.join(__dirname + '/paths/' + file),
+      'utf-8'
+    );
+    const reqDoc = YAML.parse(fileContent);
+    swaggerPaths = {
+      ...swaggerPaths,
+      ...reqDoc,
+    };
+  });
+}
+readYMLFiles();
 
 export default {
   openapi: '3.0.0',
@@ -25,11 +46,16 @@ export default {
     title: 'Ecom-mern-backend',
     description: 'Swagger Docs for backend',
   },
-  paths,
   servers: [
     {
       url: ENV_VARS.swagger_url,
       description: 'Same host which the serves the backend.',
     },
   ],
+  paths: swaggerPaths,
+  components: {
+    responses,
+    schemas,
+    securitySchemes,
+  },
 };
