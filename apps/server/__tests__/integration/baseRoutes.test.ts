6471b4df3aeae7c88ccdbbe73f07f2b69300568f
Nishant <nishkohli96@gmail.com>
2024-10-11 02:18:08 +0530

    supertest basic routes wip

--- apps/server/__tests__/integration/baseRoutes.test.ts
+++ apps/server/__tests__/integration/baseRoutes.test.ts
@@ -1,1 +1,21 @@
 import superTest from 'supertest';
+import httpStatus from 'http-status';
+import expressApp from '../../src/app';
+import { getFakeWord } from '../utils';
+
+const app = superTest(expressApp);
+const fakeRoute = getFakeWord();
+
+describe('Test Home & 404 Routes', () => {
+  /* API Route */
+  test('GET: \\', async () => {
+    const response = await app.get('/api');
+    expect(response.status).toEqual(httpStatus.OK);
+  });
+
+  /* 404 Route */
+  test(`GET: \\${fakeRoute}`, async () => {
+    const response = await app.get(`/api/${fakeRoute}`);
+    expect(response.status).toEqual(httpStatus.NOT_FOUND);
+  });
+});
--- apps/server/jest.config.ts
+++ apps/server/jest.config.ts
@@ -158,7 +158,7 @@ const config: Config = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-node",
+  testEnvironment: 'node',
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -173,9 +173,7 @@ const config: Config = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: ['/node_modules/', '/__tests__/utils/'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
@@ -191,8 +189,8 @@ const config: Config = {
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
-  //   "/node_modules/",
-  //   "\\.pnp\\.[^\\/]+$"
+  //   '/node_modules/',
+  //   '/tests/utils/.*\\.(js|ts)$'
   // ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
